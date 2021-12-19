823f09fd4c273ac86bd793cbec40543be796dd7c
Tachbir Dewan <71145499+Md-Tachbir@users.noreply.github.com>
2021-12-19 10:59:20 +0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: 'First App',
-      home: Scaffold(body: MyApp()),
+      home: Scaffold(body: MyApp2()),
     ),
   );
 }
 
+class MyApp2 extends StatefulWidget {
+  const MyApp2({Key? key}) : super(key: key);
 
+  @override
+  _MyApp2State createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('Hello statefull widget')),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -58,4 +70,3 @@ class CustomButton extends StatelessWidget {
     );
   }
 }
-
