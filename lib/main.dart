ca5db2138977cfe63225193a58c2cf6126a59901
Tachbir Dewan <71145499+Md-Tachbir@users.noreply.github.com>
2021-12-19 11:56:43 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      title: 'First App',
+      title: 'Second App',
       home: Scaffold(body: MyApp2()),
     ),
   );
@@ -17,56 +17,32 @@ class MyApp2 extends StatefulWidget {
 }
 
 class _MyApp2State extends State<MyApp2> {
+  String txt = "";
+  @override
+  void initState() {
+    txt = "Hello statefull widget";
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text('Hello statefull widget')),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CustomButton("Ok"),
-        SizedBox(
-          height: 10,
-        ),
-        CustomButton("Save"),
-        SizedBox(
-          height: 10,
-        ),
-        CustomButton("Click"),
-        SizedBox(
-          height: 10,
-        ),
-        CustomButton("No"),
-      ],
-    ));
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  final String title;
-
-  CustomButton(this.title);
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        print('onTap');
-      },
-      child: Container(
-        height: 40,
-        width: 100,
-        decoration:
-            BoxDecoration(shape: BoxShape.rectangle, color: Colors.blue),
-        child: Center(child: Text(title)),
-      ),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(txt),
+          RaisedButton(
+            onPressed: () {
+              setState(() {
+                txt = "Messege changed";
+              });
+              print("onpressed $txt");
+            },
+            child: Text("change"),
+          )
+        ],
+      )),
     );
   }
 }
